4dba2f89fb198c2fdcb3527fc915ddfbd8a0b068
AtulMinz <atulminz.jsr@gmail.com>
2024-05-28 13:46:53 +0530

    merged

---
+++ src/app/components/ConnectWallet.tsx
@@ -0,0 +1,16 @@
+import { ConnectKitButton } from "connectkit";
+import { Button } from "@/components/ui/button";
+
+export const ConnectWallet = () => {
+  return (
+    <ConnectKitButton.Custom>
+      {({ isConnected, show, truncatedAddress }) => {
+        return (
+          <Button onClick={show}>
+            {isConnected ? truncatedAddress : "Connect Wallet"}
+          </Button>
+        );
+      }}
+    </ConnectKitButton.Custom>
+  );
+};
