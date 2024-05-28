1c5c6f98eabe81b94f91316a4b60eb8ddb40aea9
AtulMinz <atulminz.jsr@gmail.com>
2024-05-28 21:11:43 +0530

    Updated Publication and CSS

--- src/app/components/Publication/Publications.tsx
+++ src/app/components/Publication/Publications.tsx
@@ -1,3 +1,5 @@
+//@ts-nocheck
+
 "use client";
 
 import { apolloClient } from "@/app/helpers/client";
@@ -7,7 +9,13 @@ import {
 } from "@lens-protocol/react-web";
 
 import { UpdateIcon } from "@radix-ui/react-icons";
-import {} from "@lens-protocol/metadata";
+import {
+  Card,
+  CardContent,
+  CardTitle,
+  CardDescription,
+  CardHeader,
+} from "@/components/ui/card";
 
 const Publication = () => {
   let {
@@ -30,34 +38,45 @@ const Publication = () => {
     return (
       <div className="min-w-full h-screen flex justify-center items-center">
         <div>
-          <UpdateIcon className="animate-spin size-12" />
+          <UpdateIcon className="animate-spin size-12 invert" />
         </div>
       </div>
     );
   }
 
   return (
-    <main className="w-fit min-h-screen">
-      <div className="flex justify-center items-center flex-col">
-        {publications?.map((publication) => (
-          <div key={publication.id} className="h-3/4 flex justify-center">
-            {publication.by.handle?.fullHandle}
-            <img
-              className="w-1/3"
-              src={
-                publication.__typename === "Post"
-                  ? publication.metadata?.asset?.image?.optimized.uri
-                  : ""
-              }
-            />
-            <div>
-              {publication.metadata?.content?.replace(
-                /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gi,
-                "[LINK]($1)"
-              )}
-            </div>
-          </div>
-        ))}
+    <main className="w-full min-h-screen bg-black">
+      <div className="flex justify-center">
+        <div className="w-[35vw] space-y-5">
+          {publications?.map((publication) => (
+            <Card>
+              <CardHeader>
+                <CardTitle
+                  key={publication.id}
+                  className="h-3/4 flex justify-center"
+                >
+                  {publication.by.handle?.fullHandle}
+                </CardTitle>
+              </CardHeader>
+              <CardContent>
+                <img
+                  className=""
+                  src={
+                    publication.__typename === "Post" || "Quote"
+                      ? publication.metadata?.asset?.image?.optimized.uri
+                      : ""
+                  }
+                />
+                <div className="mt-4 break-words">
+                  {publication.metadata?.content?.replace(
+                    /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gi,
+                    "[LINK]($1)"
+                  )}
+                </div>
+              </CardContent>
+            </Card>
+          ))}
+        </div>
       </div>
     </main>
   );
