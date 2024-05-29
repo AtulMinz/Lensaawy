a6c1577edce6aa7b57e3ccbfd9ecc2de45303652
AtulMinz <atulminz.jsr@gmail.com>
2024-05-29 23:00:38 +0530

    Few tweaks

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { ConnectWallet } from "@/app/components/ConnectWallet";
 import Publication from "./components/Publication/Publications";
 import { Suspense } from "react";
+import Profiles from "./components/Profile/Profiles";
 
 export default function Home() {
   return (
@@ -8,7 +9,8 @@ export default function Home() {
       <Suspense fallback={"Loading"}>
         <ConnectWallet />
       </Suspense>
-      <Publication />
+      {/* <Publication /> */}
+      <Profiles localname="stani" />
     </div>
   );
 }
