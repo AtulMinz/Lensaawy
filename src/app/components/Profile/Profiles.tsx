10ad071541b0809e688237972d82c35dd74ed93d
AtulMinz <atulminz.jsr@gmail.com>
2024-05-29 23:00:29 +0530

    Added Profile section

--- src/app/components/Profile/Profiles.tsx
+++ src/app/components/Profile/Profiles.tsx
@@ -1,5 +1,38 @@
+/* eslint-disable @next/next/no-img-element */
 "use client";
+import { useProfile, usePublication } from "@lens-protocol/react-web";
+import React from "react";
 
-const Profiles = () => {};
+interface Props {
+  localname: string;
+}
+
+const Profiles: React.FC<Props> = ({ localname }) => {
+  const { data: profile, error } = useProfile({
+    forHandle: `lens/${localname}`,
+  });
+
+  return (
+    <>
+      {profile?.metadata?.picture?.__typename === "ImageSet" && (
+        <>
+          <div>
+            <img
+              src={
+                profile.metadata.coverPicture?.__typename === "ImageSet"
+                  ? profile.metadata.coverPicture.optimized?.uri
+                  : ""
+              }
+              alt="Cover"
+            />
+          </div>
+          <div>
+            <img src={profile.metadata.picture.optimized?.uri} alt="Profile" />
+          </div>
+        </>
+      )}
+    </>
+  );
+};
 
 export default Profiles;
